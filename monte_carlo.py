2793704b78799e98ddc645d4b2a1a11bb2f86dbd
narnanam <narnanam@gmail.com>
2024-02-09 18:56:23 +0200

    7

--- monte_carlo.py
+++ monte_carlo.py
@@ -11,7 +11,20 @@ for _ in range(num_trials):
 
 probabilities = {i: count / num_trials for i, count in sum_counts.items()}
 
-print("Сума | Ймовірність")
-print("--- | ---")
+l = list()
+l.append(2.78)
+l.append(5.56)
+l.append(8.33)
+l.append(11.11)
+l.append(13.89)
+l.append(16.67)
+l.append(13.89)
+l.append(11.11)
+l.append(8.33)
+l.append(5.56)
+l.append(2.78)
+print("Сума | Експериментальна ймовірність | Істина ймовірність | Різниця")
+print("--- | --- | --- | ---")
 for total, probability in probabilities.items():
-    print(f"{total} | {probability*100:.2f}")
+    true_probabilities = l.pop()
+    print(f"{total} | {probability*100:.2f} | {true_probabilities} | {true_probabilities-probability*100:.2f}")
